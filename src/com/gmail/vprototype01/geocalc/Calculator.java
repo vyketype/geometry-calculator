5f8a1165f0e89b3f4f37b3a4844628bcf36573dd
vPrototype <65795488+vyketype@users.noreply.github.com>
2021-03-09 16:30:36 -0500

    Update .gitignore and TODO

--- src/com/gmail/vprototype01/geocalc/Calculator.java
+++ src/com/gmail/vprototype01/geocalc/Calculator.java
@@ -2,6 +2,10 @@ package com.gmail.vprototype01.geocalc;
 
 public class Calculator {
 
-    
+    /* TODO
+        areas of square, rectangle, circle (disc), losange, triangle, parallelogram, ellipse,
+        trapezoid, any regular polygon...
+        also find missing variables in formulas
+     */
 
 }
